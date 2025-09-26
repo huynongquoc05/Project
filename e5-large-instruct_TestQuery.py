8efc75b36522adae9458f47319a2c740c81f5745
huynongquoc05 <160751216+huynongquoc05@users.noreply.github.com>
2025-09-26 08:37:41 +0700

    Update e5-large-instruct_TestQuery.py

--- e5-large-instruct_TestQuery.py
+++ e5-large-instruct_TestQuery.py
@@ -1,26 +1,19 @@
 # Load from disk
 from langchain.vectorstores import FAISS
+from langchain_google_genai import GoogleGenerativeAI
 from langchain_huggingface import HuggingFaceEmbeddings
 
-db=FAISS.load_local("vector_db2", embeddings = HuggingFaceEmbeddings(model_name="intfloat/multilingual-e5-large-instruct", )
+db=FAISS.load_local("vector_db2chunk_nltk", embeddings = HuggingFaceEmbeddings(model_name="intfloat/multilingual-e5-large-instruct", )
 ,allow_dangerous_deserialization=True)
-# Example query
-query= "Nhập dữ liệu từ bàn phím trong java"
-
-# results = db.similarity_search_with_score(query, k=3)
-# print("query:", query)
-# for r, score in results:
-#     print(f"Score: {score:.4f}")
-#     print(r.page_content[:500])
-#     print("-" * 80)
-
-# Example query 2
-retriewer=db.as_retriever()
-docs=retriewer.get_relevant_documents(query)
-for r in docs:
-    print(r.page_content)
+# # Example query
+query= """Nhập dữ liệu từ bàn phím trong Java
+# """
+#
+#
+results = db.similarity_search_with_score(query, k=10)
+print("query:", query)
+for r, score in results:
+    print(len(r.page_content))
+    print(f"Score: {score:.4f}")
+    print(r.page_content[:])
     print("-" * 80)
-
-
-
-
